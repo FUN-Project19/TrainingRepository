8bb40883525352c0a801646fd179a3f28974a3c9
advnacedbear <advanced.bear@gmail.com>
2015-06-26 16:30:03 +0900

    First commit from Eclipse.

---
+++ TrainingProject/src/GitTraining/training.java
@@ -0,0 +1,11 @@
+package GitTraining;
+
+public class training {
+	public int calc(int x, int y){
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		System.out.printf("Hello World!");
+	}
+}
