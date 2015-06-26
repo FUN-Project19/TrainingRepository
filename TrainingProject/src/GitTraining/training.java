cbf88a89afee6b263af2c19ae0f2e1f146a53799
advanced bear <advanced.bear@gmail.com>
2015-06-26 17:29:46 +0900

    Update training.java

--- TrainingProject/src/GitTraining/training.java
+++ TrainingProject/src/GitTraining/training.java
@@ -1,11 +1,11 @@
 package GitTraining;
 
 public class training {
-	public int calc(int x, int y){
+	public static int calc(int x, int y){
 		return 0;
 	}
 	
 	public static void main(String[] args){
-		System.out.printf("Hello World!");
+		System.out.printf("Hello World!\n");
 	}
 }
