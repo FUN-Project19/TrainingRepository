b117c31dc711ae775c4ccc1371a8f2fe3a8022af
advnacedbear <advanced.bear@gmail.com>
2015-06-26 17:38:48 +0900

    calcメソッドを実装し、mainメソッド内で出力するようにした。

--- TrainingProject/src/GitTraining/training.java
+++ TrainingProject/src/GitTraining/training.java
@@ -1,11 +1,13 @@
 package GitTraining;
 
 public class training {
-	public int calc(int x, int y){
-		return 0;
+	public static int calc(int x, int y){
+		return x+y;
 	}
 	
 	public static void main(String[] args){
-		System.out.printf("Hello World!");
+		System.out.printf("Hello World!\n");
+		String s = "3 + 4 = "+calc(3,4);
+		System.out.printf(s);
 	}
 }
